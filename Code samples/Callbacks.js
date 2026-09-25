7ab76fefc49717183733e5a338fd1b610848f6b3
Pedro20-lab <pedroburgos988@gmail.com>
2026-09-24 23:49:11 -0500

    More on promises

---
+++ Code samples/Callbacks.js
@@ -0,0 +1,7 @@
+console.log('Starting timer...');
+
+setTimeout(() => {
+    console.log('6 seconds have passed');
+}, 6000)
+
+console.log('This will be printed before the timer callback executes');
