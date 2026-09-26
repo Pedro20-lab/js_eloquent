89a6f4e6c24939add70309b73951f7763d3ca712
Pedro Burgos <pedroburgos988@gmail.com>
2026-09-25 23:47:28 -0500

    Promises sequencially

--- Promises.js
+++ Promises.js
@@ -35,19 +35,19 @@
 //   return nextDigit("", 0);
 // }
 
-fetch("http://127.0.0.1:8000/api/guests") 
-  .then(response => { 
+fetch("http://127.0.0.1:8000/api/guests")
+  .then(response => {
     if (!response.ok) {
       console.log(`Response is this ${response.status}`)
-      return null; 
+      return null;
     }
-  
+
     let type = response.headers.get("content-type");
-  
+
     if (type !== "application/json") {
       throw new TypeError(`Expected JSON, got ${type}`);
     }
-    
+
     return response.json();
   })
   .then(data => { // Called with the parsed response body or null
@@ -145,4 +145,41 @@ function fetchSequentially(urls) {
   // bodies array. Note that we don't include any error handlers:
   // we want to allow errors to propagate to the caller.
   return p.then(() => bodies);
+}
+
+// This function takes an array of input values and a "promiseMaker" function.
+// For any input value x in the array, promiseMaker(x) should return a Promise
+// that will fulfill to an output value. This function returns a Promise
+// that fulfills to an array of the computed output values.
+//
+// Rather than creating the Promises all at once and letting them run in
+// parallel, however, promiseSequence() only runs one Promise at a time
+// and does not call promiseMaker() for a value until the previous Promise
+// has fulfilled.
+function promiseSequence(inputs, promiseMaker) {
+  // Make a private copy of the array that we can modify
+  inputs = [...inputs];
+  // Here's the function that we'll use as a Promise callback
+  // This is the pseudorecursive magic that makes this all work.
+  function handleNextInput(outputs) {
+    if (inputs.length === 0) {
+      // If there are no more inputs left, then return the array
+      // of outputs, finally fulfilling this Promise and all the
+      // previous resolved-but-not-fulfilled Promises.
+      return outputs;
+    } else {
+      // If there are still input values to process, then we'll
+      // return a Promise object, resolving the current Promise
+      // with the future value from a new Promise.
+      let nextInput = inputs.shift(); // Get the next input value,
+      return promiseMaker(nextInput) // compute the next output value,
+        // Then create a new outputs array with the new output value
+        .then(output => outputs.concat(output))
+        // Then "recurse", passing the new, longer, outputs array
+        .then(handleNextInput);
+    }
+  }
+  // Start with a Promise that fulfills to an empty array and use
+  // the function above as its callback.
+  return Promise.resolve([]).then(handleNextInput);
 }
